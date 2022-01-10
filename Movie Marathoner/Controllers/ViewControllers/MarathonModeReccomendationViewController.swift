a52a36c1cd97eb7d2002d01a70a9b6911f1dbf58
Andrew Saeyang <a09saeyang@gmail.com>
2022-01-10 15:41:04 -0800

    added view controller for marathon mode VC

---
+++ Movie Marathoner/Controllers/ViewControllers/MarathonModeReccomendationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MarathonModeReccomendationViewController.swift
+//  Movie Marathoner
+//
+//  Created by Andrew Saeyang on 1/10/22.
+//
+
+import UIKit
+
+class MarathonModeReccomendationViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}// End of class

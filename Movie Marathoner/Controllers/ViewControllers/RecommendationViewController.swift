457efbf37191dd1fba439d96a88c8139b23c5046
Andrew Saeyang <a09saeyang@gmail.com>
2022-01-14 12:28:33 -0800

    icon images and splash screen image added. First build for testing

--- Movie Marathoner/Controllers/ViewControllers/RecommendationViewController.swift
+++ Movie Marathoner/Controllers/ViewControllers/RecommendationViewController.swift
@@ -7,12 +7,20 @@
 
 import UIKit
 
-class RecommendationViewController: UIViewController {
+class RecommendationViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     // MARK: - Outlets
     
+    @IBOutlet weak var runTimeLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     // MARK: - Properties
-    var movie: Movie?
+    var movie: Movie?{
+        didSet{
+            updateView()
+        }
+    }
     
     var marathonTime: Int = -1
     private var finalRunTime = -1
@@ -24,7 +32,10 @@ class RecommendationViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateView()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        //updateView()
         
     }
     
@@ -58,26 +69,11 @@ class RecommendationViewController: UIViewController {
         MovieAPIController.translateData(with: movieData) { result in
             self.movieRecommendations = result
             self.movieRecommendations.shuffle()
-            self.continueSetup()
+            self.configureRecommendation()
         }
     }
-    
-    func setupReccomendation(){
-        
-    }
-    
-    func printCheck(with movies: [Movie]) -> Void{
-        for movie in movies{
-            if let title = movie.originalTitle{
-                print("\(title) Successfully fetched. Runtime \(movie.runtime!)")
-            } else {
-                print("\(movie.id!) Missing")
-            }
-        }
-    }
-    
-    
-    func continueSetup(){
+
+    func configureRecommendation(){
         marathonTime = marathonTime * 60
         
         for movie in movieRecommendations{
@@ -87,12 +83,15 @@ class RecommendationViewController: UIViewController {
                 if doesTimeFit <= marathonTime{
                     finalRecommendation.append(movie)
                     finalRunTime += runTime
-                    print("Current runtime:\(countRunTime(for: finalRecommendation)) with \(finalRecommendation.count) movies")
+                    print("Current runtime: \(countRunTime(for: finalRecommendation)) with \(finalRecommendation.count) movies")
+                    
                 }
             }
         }
         
         print(countRunTime(for: finalRecommendation))
+        runTimeLabel.text = "Total runtime: \(printRunTime())"
+        collectionView.reloadData()
     }
     
     func countRunTime(for marathon: [Movie]) -> Int {
@@ -105,4 +104,62 @@ class RecommendationViewController: UIViewController {
         }
         return retVal
     }
+    
+    func printRunTime() -> String{
+       
+        let minuites = finalRunTime%60
+        let hours = finalRunTime/60
+        
+        return "\(hours) hours \(minuites) minuites"
+    }
+    
+    func printCheck(with movies: [Movie]) -> Void{
+        for movie in movies{
+            if let title = movie.originalTitle{
+                print("\(title) Successfully fetched. Runtime \(movie.runtime!)")
+            } else {
+                print("\(movie.id!) Missing")
+            }
+        }
+    }
 }// End of class
+
+extension RecommendationViewController{
+    // MARK: UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return finalRecommendation.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as? RecommendationCollectionViewCell else { return UICollectionViewCell()}
+        
+        cell.movie = finalRecommendation[indexPath.row]
+        //cell.delegate = self
+        return cell
+    }
+}
+
+//MARK: - Collection View Flow Layout Delegate Methods
+extension RecommendationViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        //we want 90% of the screen to be used, / 2 - 45%
+        
+        let width = view.frame.width * 0.45
+        
+        return CGSize(width: width, height: width * 3/2 )
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        let width = view.frame.width * 0.45
+        let cellsTotalWidth = width * 2
+        let leftOverWidth = view.frame.width - cellsTotalWidth
+        let inset = leftOverWidth / 3
+        //insets == padding
+        return UIEdgeInsets(top: inset, left: inset, bottom: 0, right: inset)
+    }
+} //End of extension
